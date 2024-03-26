eb65fc18793d8020eecb09c76282e95be0991560
ihebghdifi <ihabghdifi@gmail.com>
2024-03-26 14:12:01 +0100

    ProfilDoctor

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,28 @@ import About from "./Pages/About.jsx";
 import {BrowserRouter as Router, Route,Routes} from  'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux' 
 import { decrement, increment } from './reducers/counter.js'
+import ProfileDoc from "./Pages/ProfileDoc.jsx";
+
+
 
 let App = () => {
   const count = useSelector((state) => console.log(state))
 const [products,setProducts]=useState([])
 const dispatch = useDispatch()
   return (
-    <div>
-      <button onClick={()=>dispatch(increment())}>increment</button>
-      <button onClick={()=>dispatch(decrement())}>decrement</button>
-      {/* <h1>{count}</h1> */}
-    </div>
-  //   <Router>
-  //    <div>
-  //     <h1>index</h1>
-  //     <Routes>
-  //       <Route exact  path="/" element={<Home/>} >  </Route>
-  //       <Route  path="/About" element={<About/>} >  </Route>
 
-  //     </Routes>
-  //   </div>
-  //  </Router>
+    <Router>
+     <div>
+      <h1></h1>
+      <Routes>
+        <Route exact  path="/" element={<Home/>} >  </Route>
+        <Route  path="/About" element={<About/>} >  </Route>
+        <Route  path="/docteur/profile" element={<ProfileDoc/>} >  </Route>
+
+
+      </Routes>
+    </div>
+   </Router>
   );
 };
 
