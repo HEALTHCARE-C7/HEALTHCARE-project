e896ec598586908d44b61e56516bd5784fcbe516
omarmrad01 <mardomar36@gmail.com>
2024-03-31 22:58:30 +0100

    done

--- client/src/components/NavBarre.jsx
+++ client/src/components/NavBarre.jsx
@@ -6,26 +6,26 @@ export default function NavBarre() {
   return (
     <nav className=" navbar navbar-expand-lg navbar-light " >
   <div className="container">
-    <div className=" col-2 col-logo"><a className="navbar-brand col-logo" href="#">Healthcare</a></div>
+    <div className=" col-2 col-logo"><a className="navbar-brand col-logo" >Healthcare</a></div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#" onClick={()=>navigate('/')}>Home</a>
+          <a className="nav-link active" aria-current="page"  onClick={()=>navigate('/')}>Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Service</a>
+          <a className="nav-link" onClick={()=>navigate('/Home2')}>Service</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#" onClick={()=>navigate('/contact')}>Contact us</a>
+          <a className="nav-link"  onClick={()=>navigate('/contact')}>Contact us</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Help</a>
+          <a className="nav-link" >Help</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Blogs</a>
+          <a className="nav-link" >Blogs</a>
         </li>
       </ul>
     </div>
--- client/src/components/PatientSignup.jsx
+++ client/src/components/PatientSignup.jsx
@@ -13,41 +13,7 @@ const Signup = () => {
     data.email = data.email.toLowerCase()
     dispatch(registerPatient(data))
   }
-//   function isValidEmail(email) {
-//     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-//     return regex.test(email);
-// }
-  // const clear=(req,res)=>{
-  //   const {firstName,lastName,email,password,gender,location,age,phoneNumber}=req.body
-  //   if(!firstName){
-  //       return res.status(400).json('write your firstName')
-  //   }
-  //   if(!lastName){
-  //       return res.status(400).json('write your lastName')
-  //   }
-  //   if(!email || !isValidEmail(email)){
-        
-  //       return res.status(400).json('Please provide a valid email address')
-  //   }
-  //   if(!password || password.length<8){
-  //       return res.status(400).json('Password must be at least 8 characters long')
-  //   }
-  //   if(!location){
-  //       return res.status(400).json('select your location')
-  //   }
-  //    if(!age){
-  //       return res.status(400).json('select your age')
-  //   } 
-  //   if(!phoneNumber){
-  //       return res.status(400).json('select your phoneNumber')
-  //   }
-  //   if(!gender){
-  //     return res.status(400).json('select your phoneNumber')
-  // }
-  // else {
-  //   navigate('/login')
-  // }
-  // }
+
     return (
     <div className="modal-content">
       <div className="signup-page">
